d48f6aa50de5ffb55baef00441b19ad4f6d3d0cd
BangbangbangbangRAi <34760495+BangbangbangbangRAi@users.noreply.github.com>
2018-05-02 23:55:43 +0800

    改了一下主界面字体

--- app/src/main/java/com/example/cj/videoeditor/activity/MainActivity.java
+++ app/src/main/java/com/example/cj/videoeditor/activity/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.cj.videoeditor.activity;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.cj.videoeditor.R;
 
@@ -17,11 +19,14 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
         Button recordBtn = (Button) findViewById(R.id.record_activity);
         Button selectBtn = (Button) findViewById(R.id.select_activity);
         Button audioBtn = (Button) findViewById(R.id.audio_activity);
-
         recordBtn.setOnClickListener(this);
         selectBtn.setOnClickListener(this);
         audioBtn.setOnClickListener(this);
-
+// 将字体文件保存在assets/fonts/目录下，在程序中通过如下方式实例化自定义字体：
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/Pacifico-Regular.ttf");
+// 应用字体
+      TextView txtshow=(TextView) findViewById(R.id.show);
+      txtshow.setTypeface(typeFace);
     }
 
     @Override
